3e10dd7c7c4c45817fcabf0b276a85b053ff65b3
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2017-11-22 20:37:17 +0100

    Les2_v4

--- Les2/main/main/main.cpp
+++ Les2/main/main/main.cpp
@@ -8,44 +8,70 @@
 #include "Module.h"
 
 
-void printStudentlijst(std::vector<Student*> studentenLijst, std::vector<Module*> moduleLijst);
-int getEC(std::vector<Module*> moduleLijst, Student* student);
+void printStudentlijst(std::vector<Student*> studentenLijst);
+void moduleToewijzen(std::vector<Student*> studentenLijst, Module* modules[]);
 
 int main()
 {
+	//create module array and modules
+	Module* modules[3] = {
+		new Module("Project Context",5),
+		new Module("Heavy Object Oriented Programming", 2),
+		new Module("Kernvak Game Design", 4)
+	};
+
 	//creates the list for all the students
 	std::vector<Student*> studentenLijst;
-	std::vector<Module*> moduleLijst;
-	//create student *make this a list*
-	Student* student1 = new Student("Bert Baas");
-	Student* student2 = new Student("Frans Froque");
-	Student* student3 = new Student("Dirk Doofus");
-	Module* module1 = new Module("Project Context",5);
+	//create students
+	studentenLijst.push_back(new Student("Bert Baas"));
+	studentenLijst.push_back(new Student("Frans Froque"));
+	studentenLijst.push_back(new Student("Dirk Doofus"));
+	studentenLijst.push_back(new Student("Ollie Ober"));
+	studentenLijst.push_back(new Student("Sarah Sievert"));
+	studentenLijst.push_back(new Student("Merel Mozart"));
+	studentenLijst.push_back(new Student("Wanda Warrig"));
+	studentenLijst.push_back(new Student("Gerard Groen"));
+	studentenLijst.push_back(new Student("Evert Elerfort"));
+	studentenLijst.push_back(new Student("Hannah Hoeder"));
 
-	module1->klassenLijst.push_back(student1);
-	//getEC(moduleLijst, student1);
 
-	studentenLijst.push_back(student1);
-	studentenLijst.push_back(student2);
-	studentenLijst.push_back(student3);
+	//printStudentlijst(studentenLijst);
+
+	moduleToewijzen(studentenLijst, modules);
+
+	//test - klassenlijst printen van alle modules
 	
-	printStudentlijst(studentenLijst, moduleLijst);
+	for (int j = 0; j < 3; j++)
+	{
+		Module* m = modules[j];
+		std::cout << " " << std::endl;
+		std::cout << m->getName() << std::endl;
+		std::cout << "------------------" << std::endl;
+
+		std::vector<Student*> lijst = m->klassenLijst;
+		std::vector<Student*>::iterator i = lijst.begin();
+		while (i != lijst.end())
+		{
+			std::cout << (*i)->getName() << std::endl;		
+			i++;
+		}
+		std::cout << "------------------" << std::endl;
+	}
+	//test
 
 	char c;
 	std::cin >> c;
     return 0;
 }
 
-void printStudentlijst(std::vector<Student*> studentenLijst, std::vector<Module*> moduleLijst)
+void printStudentlijst(std::vector<Student*> studentenLijst)
 {
 	std::cout << "------------------" << std::endl;
 	std::vector<Student*>::iterator i = studentenLijst.begin();
 
 	while (i != studentenLijst.end())
 	{
-
 		std::cout << (*i)->getName() << std::endl;
-		std::cout << getEC(moduleLijst, (*i)) << std::endl;
 		std::cout << "------------------" << std::endl;
 		i++;
 
@@ -53,24 +79,32 @@ void printStudentlijst(std::vector<Student*> studentenLijst, std::vector<Module*
 
 }
 
-int getEC(std::vector<Module*> moduleLijst, Student* student)
+void moduleToewijzen(std::vector<Student*> studentenLijst, Module* modules[])
 {
-	int totaalEC = 0;
-
-	std::vector<Module*>::iterator i = moduleLijst.begin();
-	while (i != moduleLijst.end())
+	std::vector<Student*>::iterator i = studentenLijst.begin();
+	//for all students in the studentlist get a random amount of modules
+	while (i != studentenLijst.end())
 	{
-		std::vector<Student*> klas = (*i)->klassenLijst;
-		std::vector<Student*>::iterator i = klas.begin();
-		while (i != klas.end())
+		int randNum = rand() % 3 + 1;
+		//than loop through it and get this number of random modules
+		for (int r = 0; r < randNum; r++)
 		{
-			if ((*i)->getName() == student->getName())
-			{
-				return 4;
+			int j = rand() % 3;
+			//add student to this random module
+
+			std::vector<Student*> lijst = modules[j]->klassenLijst;
+			std::vector<Student*>::iterator q = lijst.begin();
+			while (q != lijst.end())
+			{ //stuck on trying to compare the name of this student with the one in the klassenlijst
+				if (*i->getName() == *q->getName())
+				{
+					modules[j]->klassenLijst.push_back(*i);
+				}
+				q++;
 			}
-			i++;
+
+			
 		}
 		i++;
 	}
-	return 0;
 }
